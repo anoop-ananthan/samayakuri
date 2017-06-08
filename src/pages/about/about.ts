e052d7476ae22311f74ab8a5c18f3203a767a72e
anoop-ananthan <anoop.ananthan@logicline.de>
2017-06-08 12:52:11 +0530

    Add file choosing for selecting sound file

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FilePath } from '@ionic-native/file-path';
 import { FileChooser } from '@ionic-native/file-chooser';
 
 @Component({
@@ -7,11 +8,34 @@ import { FileChooser } from '@ionic-native/file-chooser';
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  private fileName: string;
 
   constructor(
     public navCtrl: NavController,
+    private filePath: FilePath,
     private fileChooser: FileChooser
-  ) {
+  ) { }
+
+  onSoundClicked() {
+    this.getSoundFilePath();
+  }
+
+  getSoundFilePath() {
+    this.fileChooser.open()
+      .catch()
+      .then((url) => {
+        console.log(`> you selected ${url}`);
+        this.resolveFilePath(url);
+      })
+  }
+
+  resolveFilePath(url) {
+    this.filePath.resolveNativePath(url)
+      .catch()
+      .then((resolvedPath) => {
+        this.fileName = resolvedPath;
+        console.log(`> resolved path is ${resolvedPath}`);
+      })
   }
 
 }
